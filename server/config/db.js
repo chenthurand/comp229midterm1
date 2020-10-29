5f3d7e5438c437bdb6c8ddb5164e0710863044ca
chenthurand <71531471+chenthurand@users.noreply.github.com>
2020-10-29 18:31:34 -0400

    Midterm Completed

---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books229"
+  "URI":"mongodb+srv://chenthuran:chenthuran@cluster0.g8mgp.mongodb.net/chenthuran?retryWrites=true&w=majority"
+
+};
